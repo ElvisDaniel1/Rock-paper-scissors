1ffdf2649bea09d364e69855617916827f8eabcd
ElvisDaniel1 <Elvisdabiedeen5@gmail.com>
2024-11-13 17:47:02 -0400

    Complete playRound function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const humanScore = 0;
-const computerScore = humanScore;
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     //Generate num between 0 and 2 incl
@@ -8,11 +8,55 @@ function getComputerChoice() {
     //Choices
     let choices = ["rock", "paper", "scissors"];
 
-    return console.log(choices[num]);
+    return (choices[num]);
 }
 
 function getHumanChoice() {
     let userChoice = prompt("Rock, Paper, Scissors....SHOOT");
     
-    return console.log(userChoice.toLowerCase());
+    return userChoice.toLowerCase();
 }
+
+function playRound(humanChoice, computerChoice) {
+    //Rules 
+    // Rock > Scissors      Scissors > Paper        Paper > Rock
+
+    // Determine winner
+    let wonRound = false;
+
+    // Handle draw
+    if (humanChoice == computerChoice) {
+        console.log("Draw!");
+        return;
+    }
+        
+    if (humanChoice == "rock" && computerChoice == "scissors") 
+        wonRound = true;
+    else if (humanChoice == "scissors" && computerChoice == "paper")
+        wonRound = true;
+    else if (humanChoice == "paper" && computerChoice == "rock")
+        wonRound = true;
+
+
+    // Notify user of round status
+    let msg = "";
+    if (wonRound) {
+        msg = `You win! ${humanChoice} beats ${computerChoice}`;
+        console.log(msg);
+        humanScore++;
+        console.log(humanScore);
+    }
+    else {
+        msg = `You lose! ${computerChoice} beats ${humanChoice}`;
+        console.log(msg);
+        computerScore++;
+        console.log(computerScore);
+    }
+        
+}
+
+//Get values for round
+const humanChoice = getHumanChoice(); 
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
