3185430ae14d2497700a402eff095974c3ee59df
ElvisDaniel1 <Elvisdabiedeen5@gmail.com>
2024-11-13 17:14:14 -0400

    Complete getHumanChoice function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const humanScore = 0;
+const computerScore = humanScore;
+
 function getComputerChoice() {
     //Generate num between 0 and 2 incl
     let num = Math.floor(Math.random() * 3);
@@ -6,4 +9,10 @@ function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
 
     return console.log(choices[num]);
+}
+
+function getHumanChoice() {
+    let userChoice = prompt("Rock, Paper, Scissors....SHOOT");
+    
+    return console.log(userChoice.toLowerCase());
 }
